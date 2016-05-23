53d9093e35e2c137b1603fe8d5756950bf0cb234
Folow <davidgalante77@gmail.com>
2016-05-24 00:57:22 +0200

    ahem

--- busmetro.c
+++ busmetro.c
@@ -1,12 +1,26 @@
 #include "dmse.h"
 
-void debarquement(Passager** ptransport,int typeTransport)
+void debarquement(Passager** ptransport,int typeTransport,int station)
 {
+    int i;
     switch(typeTransport)
     {
         case BUS:
+        for(i=0;i<BUS;i++)
+        {
+            if(ptransport[i]->stationA==station || (ptransport->stationA==5 && station==0))
+            {
+                printf("bus : debarque le passager %d",ptransport[i]->id);
+                if(ptransport->stationA==5 && station>5)
+                {
+                    printf("bus : transfert passager le passager %d vers station ",ptransport[i]->id,5);
+                }
+                Passager_free(ptransport[i]);
+            }
+        }
         break;
-
+        case METRO:
+        break;
 
     }
 
