c6cc0d03c382f0b72838223e5edce836bd4724b7
Folow <davidgalante77@gmail.com>
2016-05-26 13:49:21 +0200

    taxiiiii

--- taxi.c
+++ taxi.c
@@ -16,6 +16,7 @@ void *threadTaxi(void *fl)
                 Passager *p=(Passager*)malloc(sizeof(Passager));
                 read(sortieTube,(Passager*)p,TAILLE_MESSAGE);
                 usleep(10);
+                printf("              .----' `-----.\n             //^^^^;;^^^^^^`\\ \n     _______//_____||_____()_\\________ \n    / %d     :      : ___              `\\ \n   |>   ____;      ;  |/\><|   ____   _<)\n  {____/    \\_________________/    \\____}\n       \\ '' /                 \\ '' /\n        '--'                   '--'\n",idTaxi);
                 printf("taxi#{%d} : passager {%d} est rendu a la station {%d}\n",idTaxi,p->id,p->stationA);
                 free(p);
             }
